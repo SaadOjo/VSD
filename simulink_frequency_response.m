c5905fc1d840edd5514536c5eff192fae242b6d9
Syed Saad Saif <saif.1853356@studenti.uniroma1.it>
2020-09-30 18:38:53 +0300

    added stuff

--- simulink_frequency_response.m
+++ simulink_frequency_response.m
@@ -7,15 +7,25 @@ opOpts = findopOptions('DisplayReport','off');
 op = findop(model,motorcycle_spec,opOpts);
 input = frest.Sinestream('Frequency',logspace(1,2,100));
 
-c = c_min;
+c_min = 1.6;
+c_max = 1.6;
+
 sysest_min = frestimate(model,op,io,input);
 
-c = c_max;
+c_min = 4.4;
+c_max = 4.4;
+
 sysest_max = frestimate(model,op,io,input);
+
+c_min = 1.6;
+c_max = 4.4;
+
+sysest_sh = frestimate(model,op,io,input);
+
 
 P = bodeoptions;
 P.FreqUnits = 'Hz';
 P.PhaseVisible = 'off';
 P.Xlim = [2, 20];
-bodeplot(sysest_min, sysest_max,'-', P);
+bodeplot(sysest_min, sysest_max, sysest_sh, P);
 
